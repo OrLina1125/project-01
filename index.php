26620f846e9fc94c38ce8c4452e90be1e19a90f0
Marina Rumy <91767944+MarinaRumy@users.noreply.github.com>
2022-03-19 09:55:32 +0900

    add array

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+//loop
+//do while loop
+//for loop//
+//foeach loop//
+$array = array("Daniel","Jane", "jacob", "Mariane");
+
+foreach($array as $loopdata){
+    echo "My name is" .$loopdata."<br>";
+}
+?>
